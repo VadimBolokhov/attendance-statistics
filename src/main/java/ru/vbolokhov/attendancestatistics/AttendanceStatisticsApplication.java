877f82eaa96f4f9eacf36f69214b8d5a444f4a81
VadimBolokhov <wad4@inbox.ru>
2019-12-20 13:33:32 +0300

    dev: spring initializr

---
+++ src/main/java/ru/vbolokhov/attendancestatistics/AttendanceStatisticsApplication.java
@@ -0,0 +1,13 @@
+package ru.vbolokhov.attendancestatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceStatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vbolokhov/attendancestatistics/AttendanceStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vbolokhov.attendancestatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
